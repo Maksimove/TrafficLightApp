c4bf3b936ef13a6c5f4ddcce72cc18a68d57c2ee
Maksimove <spamw7@icloud.com>
2024-11-25 22:31:17 +0300

    Initial Commit

---
+++ TrafficLightApp/TrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightAppApp.swift
+//  TrafficLightApp
+//
+//  Created by Evgeniy Maksimov on 25.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
