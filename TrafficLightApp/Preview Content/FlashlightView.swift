3389ef95da893e5c5b295f2a41459980efb2f338
Maksimove <spamw7@icloud.com>
2024-11-25 22:36:15 +0300

    Create trafficlight

---
+++ TrafficLightApp/Preview Content/FlashlightView.swift
@@ -0,0 +1,18 @@
+//
+//  FlashlightView.swift
+//  TrafficLightApp
+//
+//  Created by Evgeniy Maksimov on 25.11.2024.
+//
+
+import SwiftUI
+
+struct FlashlightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlashlightView()
+}
